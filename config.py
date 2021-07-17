a56c24e7ab78e487ee7f50d07cf8ac784f15f07b
Kai <kaiyhirota@gmail.com>
2021-07-17 19:49:47 +1000

    created logging directory

--- config.py
+++ config.py
@@ -1,3 +1,9 @@
+import os
+from loguru import logger
+
+LOG_DIR = "logs"
+logger.add(os.path.join(LOG_DIR, "file_{time}.log"))
+
 # 5 minute moving average
 MOVING_AVERAGE_WINDOW = 60 * 5
 
@@ -9,4 +15,4 @@ OUTPUT_FILE = "output/output.jsonl"
 if VERBOSE is set to True, new conversation rate, moving average, and 
 percentage change will also be included in the output jsonlines file.
 """
-VERBOSE = True
+VERBOSE = False
