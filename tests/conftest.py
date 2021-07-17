cefbd2c85376b11be31e05317cdf1f95b20e8062
Kai <kaiyhirota@gmail.com>
2021-07-18 03:33:19 +1000

    add caplog for testing logging output

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,10 @@
+import logging
 import os
 from pathlib import Path
 
+from loguru import logger
 import pytest
+from _pytest.logging import caplog as _caplog
 
 from conversion_rate_analyzer.models.currency_conversion_rate import CurrencyConversionRate
 
@@ -17,3 +20,14 @@ def sample_input_path() -> str:
 def conversion_rate() -> CurrencyConversionRate:
     obj = {"timestamp": 1554933784.023, "currencyPair": "CNYAUD", "rate": 0.39281}
     return CurrencyConversionRate.parse_obj(obj)
+
+
+@pytest.fixture
+def caplog(_caplog):
+    class PropogateHandler(logging.Handler):
+        def emit(self, record):
+            logging.getLogger(record.name).handle(record)
+
+    handler_id = logger.add(PropogateHandler(), format="{message} {extra}")
+    yield _caplog
+    logger.remove(handler_id)
